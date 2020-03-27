ed1c3a7abac4a0d56c004d7542b4fb51c718f869
nimeshchudasama <34605005+nimeshchudasama@users.noreply.github.com>
2020-03-27 11:32:28 -0500

    Update preprocessMethod.py

--- preprocessMethod.py
+++ preprocessMethod.py
@@ -9,87 +9,87 @@ from PIL import Image
 import imutils
 from skimage.filters import threshold_local
 
+
 def order_points(pts):
-	# initialzie a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype = "float32")
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
-	s = pts.sum(axis = 1)
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(pts, axis = 1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
-	# return the ordered coordinates
-	return rect
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect
+
 
 def four_point_transform(image, pts):
-	# obtain a consistent order of the points and unpack them
-	# individually
-	rect = order_points(pts)
-	(tl, tr, br, bl) = rect
-	# compute the width of the new image, which will be the
-	# maximum distance between bottom-right and bottom-left
-	# x-coordiates or the top-right and top-left x-coordinates
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
-	# compute the height of the new image, which will be the
-	# maximum distance between the top-right and bottom-right
-	# y-coordinates or the top-left and bottom-left y-coordinates
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
-	# now that we have the dimensions of the new image, construct
-	# the set of destination points to obtain a "birds eye view",
-	# (i.e. top-down view) of the image, again specifying points
-	# in the top-left, top-right, bottom-right, and bottom-left
-	# order
-	dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
-	# compute the perspective transform matrix and then apply it
-	M = cv2.getPerspectiveTransform(rect, dst)
-	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-	# return the warped image
-	return warped
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    # return the warped image
+    return warped
+
 
 def runPreprocess(image_dir):
     imageCount = 100
     images = []
-    tempImages = [] 
+    tempImages = []
     times = []
-    
+
     # Load intermediate set into images list
     for i in range(imageCount):
-        images.append(str(image_dir) + "/W2_XL_input_noisy_" + str(1000 + i) + "")
-        
-        
+        images.append(str(image_dir) + "/W2_XL_input_noisy_" + str(1000 + i) + ".jpg")
+
     # Preprocess the images and store them in a temp list
     for i in range(len(images)):
         startTime = int(round(time.time() * 1000))
         # Open the image file
         tempImage = cv2.imread(images[i])
-        print(images[i])
-        
         ratio = tempImage.shape[0] / 500.0
         orig = tempImage.copy()
-        tempImage = imutils.resize(tempImage, height = 500)
+        tempImage = imutils.resize(tempImage, height=500)
         gray = cv2.cvtColor(tempImage, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (5, 5), 0)
         edged = cv2.Canny(gray, 75, 200)
-        
-        snts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
-        cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
         # loop over the contours
         for c in cnts:
             # approximate the contour
@@ -100,44 +100,44 @@ def runPreprocess(image_dir):
             if len(approx) == 4:
                 screenCnt = approx
                 break
-        
+
         warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
         warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-        
-        T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+
+        T = threshold_local(warped, 11, offset=10, method="gaussian")
         warped = (warped > T).astype("uint8") * 255
-        
+
         # The preprocesed images are saved temporarily in memory instead of written into output directory
         # so calculating the actual processing time won't be affected
         tempImages.append(tempImage)
-        
+
         # Record elapsed processing time for the image
         times.append(int(round(time.time() * 1000)) - startTime)
-        
+
     print("Total processing time: ", sum(times), "ms")
-    print("Average processing time: ", sum(times)/len(times), "ms")
-        
+    print("Average processing time: ", sum(times) / len(times), "ms")
+
     return tempImages
-        
+
 
 def main():
     image_dir = "intermediate"
-    
+
     # Preprocess the images
     processedImages = runPreprocess(image_dir)
-    
-    
+
     # Output processed images into output directory
     output_dir = "results"
     try:
         os.makedirs(output_dir)
     except FileExistsError:
         pass
-        
+
     for i in range(len(processedImages)):
         tempImage = processedImages[i]
         tempImage.save(output_dir + "/W2_XL_input_noisy_" + str(1000 + i) + ".jpg")
-        
+
     print("Saved processed images to results directory")
+
 
 main()

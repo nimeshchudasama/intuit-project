59726389619141a0eeac36798b02c44255ea06a8
nimeshchudasama <34605005+nimeshchudasama@users.noreply.github.com>
2020-03-31 18:49:00 -0500

    Update preprocessMethod.py
    Current issue is that on image 13, it detects more than 4 coordinates for the image and it fails. Anyone wanna help me fix it?

--- preprocessMethod.py
+++ preprocessMethod.py
@@ -34,7 +34,6 @@ def runPreprocess(image_dir):
         img = cv2.imread(images[i])
 
 
-
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (3, 3), 0)
         thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
